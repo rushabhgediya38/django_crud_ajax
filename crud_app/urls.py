625c064c0ec50a6341182abc0b0228dd6899c7fc
rushabh <rushabhgediya38@gmail.com>
2021-02-13 12:21:23 +0530

    Add Update-delete

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('save/', save, name='save'),
+    path('delete/', data_delete, name='delete'),
+    path('edit/', data_edit, name='edit'),
 ]
 
--- crud_app/views.py
+++ crud_app/views.py
@@ -15,16 +15,41 @@ def index(request):
 
 def save(request):
 	if request.method == 'POST':
+		stu = request.POST.get('stuid')
 		name = request.POST['name_1']
 		email = request.POST['email_1']
 		description = request.POST['description_1']
 
-		ab = crud_post(name=name, email=email, description=description)
+		if stu == '':
+			ab = crud_post(name=name, email=email, description=description)
+		else:
+			ab = crud_post(id=stu, name=name, email=email, description=description)
 		ab.save()
 
 		crud_data = crud_post.objects.values()
 		all_data = list(crud_data) 
 
 		return JsonResponse({'status': 'Save', 'crud_data': all_data})
-	
+	return JsonResponse({'status': 0})
+
+def data_delete(request):
+	if request.method == "POST":
+		id = request.POST.get('sid')
+		da = crud_post.objects.get(pk=id)
+		da.delete()
+		return JsonResponse({'status':1})
+
+	return JsonResponse({'status':0})
+
+
+def data_edit(request):
+	if request.method == "POST":
+		id = request.POST.get('sid')
+		ed = crud_post.objects.get(pk=id)
+
+		ed_data = {"id":ed.id, "name":ed.name, "email":ed.email, "description":ed.description}
+
+		return JsonResponse(ed_data)
+
+
 
